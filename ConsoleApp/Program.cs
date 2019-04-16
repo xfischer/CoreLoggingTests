960c6a3c39bbb826590e262c4afe83216b81907c
Xavier Fischer <xavier@fischer.ninja>
2019-04-16 19:28:08 +0200

    Initial commit working !

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
+using MyNetStandardLib;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);                
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var myService = serviceProvider.GetService<MyService>();
+            myService.DoSomething();
+
+
+            Console.ReadLine();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddLogging(config =>
+            {
+                config.AddDebug();
+                config.AddConsole();
+            })
+            .Configure<LoggerFilterOptions>(options =>
+            {
+                options.AddFilter<DebugLoggerProvider>(null, LogLevel.Information);
+                options.AddFilter<ConsoleLoggerProvider>(null, LogLevel.Information);
+            })
+            .AddTransient<MyService>();
+            
+        }
+
+
+    }
+}
---
+++ MyNetStandardLib/MyService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MyNetStandardLib
+{
+    public class MyService
+    {
+        private readonly ILogger<MyService> _logger;
+        public MyService(ILogger<MyService> logger)
+        {
+            _logger = logger;
+            if (_logger == null)
+            {
+                Console.WriteLine("Logger is null (Console)");
+                Trace.TraceWarning("Logger is null (Trace)");
+                Debug.WriteLine("Logger is null (Debug)");
+            } else
+            {
+                _logger.LogDebug("Logger OK (logger)");
+            }
+        }
+
+        public void DoSomething()
+        {
+            _logger.LogDebug("DoSometing DEBUG message");
+            _logger.LogTrace("DoSometing TRACE message");
+            _logger.LogInformation("DoSometing INFO message");
+            _logger.LogWarning("DoSometing WARN message");
+            _logger.LogError("DoSometing ERROR message");
+            _logger.LogCritical("DoSometing CRITICAL message");
+
+        }
+    }
+}
